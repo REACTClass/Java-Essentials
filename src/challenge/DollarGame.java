3fcb56e5d6395c1fe85c59d7435c730b92b2c513
REACTClass <21jamoon@gmail.com>
2023-05-15 20:24:03 -0400

    My solution to the number game challenge

---
+++ src/challenge/DollarGame.java
@@ -0,0 +1,44 @@
+package challenge;
+
+import java.util.Scanner;
+
+public class DollarGame {
+    public static void main(String[] args) {
+
+        double pennies;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many pennies would you like?");
+        pennies = scanner.nextDouble();
+
+        double nickles;
+        System.out.println("How many nickles would you like?");
+        nickles = scanner.nextDouble();
+
+        double dimes;
+        System.out.println("How many dimes would you like?");
+        dimes = scanner.nextDouble();
+
+        double quarters;
+        System.out.println("How many quarters would you like?");
+        quarters = scanner.nextDouble();
+
+        double totalPennies = pennies * 0.01;
+        double totalNickles = nickles * .05;
+        double totalDimes = dimes * 0.1;
+        double totalQuarters = quarters * 0.25;
+
+        double total = totalPennies + totalNickles + totalDimes + totalQuarters;
+
+        if(total == 1) {
+            System.out.println("Congrats! You win!!! Your total equals $1 exactly");
+        } else if(total < 1) {
+            double changeShort = 1 - total;
+//            System.out.println(total);
+            System.out.println("You lose! You are: " + changeShort + " short.");
+        } else {
+            double changeOver = total - 1;
+//            System.out.println(total);
+            System.out.println("You lose! You are: " + changeOver + " over.");
+        }
+    }
+}
